cd7ee3692a78db1bf4d6bdfea3085a6d485755dc
Raquel R <raquelrh@gmail.com>
2018-02-25 17:56:47 +0100

    Se elimina el fichero porque no es necesario

--- model.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//Implementamos los controladores de las rutas (endpoints)
-exports.showResult = () => {
-    console.log("Mostrando los resultados desde MODEL");
-
-    return;
-};
